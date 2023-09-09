b22cf3aaf70a07a152708313af56b4e56560458e
E.M Supun Rathnayake <supun.rathnayake007@gmail.com>
2023-09-09 21:14:38 +0530

    Deploy home page -sure not complate but fine

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,15 @@
 import './index.scss'
 import Sidebar from '../Sidebar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <Outlet />
+      </div>
+    </div>
   )
 }
 export default Layout
